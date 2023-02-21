f088610839c09c4a37ed43c67923d9ccffd61841
MKyrychenko <mkyrychenko@luxoft.com>
2023-02-21 11:25:11 +0100

    try to add swagger support + updatre Spring version

--- server/src/main/java/com/ourstocks/jwtapp/config/WebConfig.java
+++ server/src/main/java/com/ourstocks/jwtapp/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.ourstocks.jwtapp.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@OpenAPIDefinition
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
